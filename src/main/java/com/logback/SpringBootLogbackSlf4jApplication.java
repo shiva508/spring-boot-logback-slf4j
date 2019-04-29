9138030ce345e2b98226c89696f8702b27b68a6c
ram <ram@ram-desktop>
2019-04-29 14:35:22 +0530

    xsas

--- src/main/java/com/logback/SpringBootLogbackSlf4jApplication.java
+++ src/main/java/com/logback/SpringBootLogbackSlf4jApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootLogbackSlf4jApplication {
-
+//sds
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLogbackSlf4jApplication.class, args);
 	}
